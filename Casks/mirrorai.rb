90872194ecb689ac04e5e1c75b58f230db4dca34
Saturnin Pugnet <saturnin.13@hotmail.fr>
2025-03-03 20:12:28 +0400

    Simplify MirrorAI cask to fix Homebrew deprecation

--- Casks/mirrorai.rb
+++ Casks/mirrorai.rb
@@ -2,13 +2,7 @@ cask "mirrorai" do
     version "latest"
     sha256 :no_check
   
-    url do
-      if Hardware::CPU.arm?
-        "https://mirror-ai-app-updates.s3.us-east-1.amazonaws.com/MirrorAI-latest-arm64.dmg"
-      else
-        "https://mirror-ai-app-updates.s3.us-east-1.amazonaws.com/MirrorAI-latest.dmg"
-      end
-    end
+    url "https://mirror-ai-app-updates.s3.us-east-1.amazonaws.com/MirrorAI-latest-arm64.dmg"
   
     name "MirrorAI"
     desc "Jarvis, but better"
